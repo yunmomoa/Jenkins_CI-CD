ffa73304c8a3e16c718435f9b7ae9a3c16ba3fda
yunmomoa <cysbunker1202@naver.com>
2025-02-13 17:35:01 +0900

    (윤성) 충돌 재커밋 02.13.목

--- frontend/react-practice-ts/src/App.tsx
+++ frontend/react-practice-ts/src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import MainPage from "./pages/MainPage";
-import Calendar from "./components/calendar/Calendar";
+import Calendar from "./pages/Calendar";
 import './App.css'
 import { ApprovalMain } from "./pages/approvalMain";
+import { ApprovalWritePage } from "./pages/approvalWritePage";
 import PersonnelMain from "./pages/PersonnelMain";
 import FormMain from "./pages/FormMain";
 import FormUpdate from "./components/form/FormUpdate";
@@ -14,7 +15,8 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/main" element={<MainPage/>} />
         <Route path="/calendar" element={<Calendar/>} />
-        <Route path="/approval" element={<ApprovalMain/>} />
+        <Route path="/approvalMain" element={<ApprovalMain />}/>
+        <Route path="/ApprovalWritePage" element={<ApprovalWritePage/>}/>
         <Route path="/personnel" element={<PersonnelMain/>} /> 
         <Route path="/form" element={<FormMain/>}>
           <Route path="detail/:formNo" element={<FormUpdate/>} />
