977a0a8561a7d739b1b88f05610901e582647098
yunmomoa <cysbunker1202@naver.com>
2025-03-04 17:41:02 +0900

    (윤성) 헤더 재푸시

--- frontend/react-practice-ts/src/components/common/Header.tsx
+++ frontend/react-practice-ts/src/components/common/Header.tsx
@@ -20,13 +20,8 @@ function Header() {
   const { pathname } = location;
   const [openModal, setOpenModal] = useState(false);
   const user = useSelector((state) => state.user);
-  
 
   const url = "http://localhost:8003/workly/uploads/profile/";
-
-  let user = useSelector((state) => {
-    return state.user;
-  });
 
   useEffect(() => {
     setPreview(user.changeName);
@@ -60,13 +55,13 @@ function Header() {
       break;
 
     case pathname.includes("AIAssistant"):
-      title = "회사규정Q&A"; 
+      title = "회사규정Q&A";
       break
-     
+
     case pathname.includes("AdminPolicyManager"):
-      title = "회사규정Q&A 관리";  
+      title = "회사규정Q&A 관리";
       break
-  
+
     default:
       title = "";
       break;
@@ -87,55 +82,42 @@ function Header() {
     }
   }
 
-      const handleLogout = () => {
-        if (confirm("로그아웃하시겠습니까?")) {
-          removeCookie("user");
-          removeCookie("accessToken");
-          dispatch(logoutUser());
-          console.log("cookie user확인: ", getCookie("user"));
-          console.log("cookie token확인: ", getCookie("accessToken"));
-          navigate("/");
-        }
+  return (
+    <>
+      {openModal &&
+        <ChangePwdModal setOpenModal={setOpenModal} />
       }
-
-      return (
-        <>
-          {openModal &&
-            <ChangePwdModal setOpenModal={setOpenModal} />
-          }
-          <header >
-            <div className={styles.header}>
-              <div className={styles.profileArea}>
-                {!preview && <img src={profileImg} alt="profile" className={styles.profileImage} />}
-                {preview && <img src={url + preview} alt="preview" className={styles.profileImage} />}
-                <div className={styles.profileInfo}>
-                  <div className={styles.profileName}>{user.userName}</div>
-                  <div className={styles.infoSection}>
-                    <div className={styles.profileRole}>{user.deptName}</div>
-                    <div className={styles.profileRole}>{user.positionName}</div>
-                  </div>
+      <header >
+        <div className={styles.header}>
+          <div className={styles.profileArea}>
+            {!preview && <img src={profileImg} alt="profile" className={styles.profileImage} />}
+            {preview && <img src={url + preview} alt="preview" className={styles.profileImage} />}
+            <div className={styles.profileInfo}>
+              <div className={styles.profileName}>{user.userName}</div>
+              <div className={styles.infoSection}>
+                <div className={styles.profileRole}>{user.deptName}</div>
+                <div className={styles.profileRole}>{user.positionName}</div>
+              </div>
+            </div>
+            <button className={styles.dropdownButton}>
+              <div>
+                <img src={down} alt="드롭다운" onClick={toggleDown} />
+              </div>
+            </button>
+            {dropDownOpen && (
+              <div className={styles.dropdownMenu}>
+                <div className={styles.menuItem} onClick={() => navigate("/myPage")}>
+                  <img src={mypage} alt="마이 페이지" />
+                  <span>마이 페이지</span>
                 </div>
-                <button className={styles.dropdownButton}>
-                  <div>
-                    <img src={down} alt="드롭다운" onClick={toggleDown} />
-                  </div>
-                </button>
-                {dropDownOpen && (
-                  <div className={styles.dropdownMenu}>
-                    <div className={styles.menuItem} onClick={() => navigate("/myPage")}>
-                      <img src={mypage} alt="마이 페이지" />
-                      <span>마이 페이지</span>
-                    </div>
-                    <div className={styles.menuItem} onClick={() => setOpenModal(true)}>
-                      <img src={changePwd} alt="비밀번호 변경" />
-                      <span>비밀번호 변경</span>
-                    </div>
-                    <div className={styles.menuItem} onClick={handleLogout}>
-                      <img src={logout} alt="로그아웃" />
-                      <span>로그아웃</span>
-                    </div>
-                  </div>
-                )}
+                <div className={styles.menuItem} onClick={() => setOpenModal(true)}>
+                  <img src={changePwd} alt="비밀번호 변경" />
+                  <span>비밀번호 변경</span>
+                </div>
+                <div className={styles.menuItem} onClick={handleLogout}>
+                  <img src={logout} alt="로그아웃" />
+                  <span>로그아웃</span>
+                </div>
               </div>
             )}
           </div>
@@ -146,6 +128,5 @@ function Header() {
       </header>
     </>
   );
-
 }
 export default Header;
