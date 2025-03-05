7381e41a9c60a4e59f2f2afed4963d833fcdd864
yebin1214 <kibin1214@naver.com>
2025-03-05 11:09:57 +0900

    전자결재 18번째 커밋(예빈)0305

--- frontend/react-practice-ts/src/hooks/useFetchNotifications.ts
+++ frontend/react-practice-ts/src/hooks/useFetchNotifications.ts
@@ -25,10 +25,10 @@ const useFetchNotifications = (userNo: number | undefined) => {
         }
         const data = await userStatusResponse.json();
         //console.log("📢 API 응답 데이터:", data); // ✅ API 응답 확인
-        // if (!Array.isArray(data) || data.length === 0) {
-        //   console.warn("❌ API 응답이 비어 있거나 배열이 아님");
-        //   return;
-        // }
+        if (!Array.isArray(data) || data.length === 0) {
+          console.warn("❌ API 응답이 비어 있거나 배열이 아님");
+          return;
+        }
         // ✅ Redux 상태 업데이트 (비동기 Thunk 호출)
         dispatch(fetchNotifications(userNo) as any);
         // 결재 완료, 반려  API
@@ -44,7 +44,8 @@ const useFetchNotifications = (userNo: number | undefined) => {
       }
     };
     fetchUserNotifications();
-    const interval = setInterval(fetchUserNotifications, 30000); // 30초마다 새로고침
+    const interval = setInterval(fetchUserNotifications, 15000); // 30초마다 새로고침
+
     return () => clearInterval(interval);
   }, [userNo, dispatch]);
   return null;
